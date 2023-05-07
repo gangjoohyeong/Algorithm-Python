0eb965e5e8bb18d86e2fabe8009c1a78d897dbf0
MS <hdcms7929@g.skku.edu>
2023-05-07 19:08:25 +0900

    9주차 풀이

---
+++ 최민수/9주차/좋다.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+number = list(map(int, sys.stdin.readline().split()))
+
+number.sort()
+count = 0
+
+for i in range(n) :
+    temp = number[: i] + number[i + 1 :]
+    left, right = 0, len(temp) - 1
+    
+    while left < right : 
+        ans = temp[left] + temp[right]
+        
+        if ans == number[i] :
+            count += 1
+            break
+            
+        elif ans < number[i] :
+            left += 1
+            
+        else : 
+            right -= 1
+            
+print(count)
