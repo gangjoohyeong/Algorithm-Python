e90579843f1920b6c8fef8ec425f90087e155e2f
kangjoohyeong <bles@kakao.com>
2023-07-03 14:17:19 +0900

    1790 commit

---
+++ 강주형/16주차/1790.py
@@ -0,0 +1,23 @@
+# 1790
+# 수 이어 쓰기 2
+
+# 타인 풀이 참고
+
+n, k = map(int, input().split())
+ans = 0
+digit = 1
+nine = 9
+
+
+while k > digit*nine:
+    k = k-(digit * nine)
+    ans = ans + nine
+    digit+=1
+    nine = nine*10
+
+ans = (ans+1) + (k-1) // digit
+
+if ans > n:
+    print(-1)
+else:
+    print(str(ans)[(k-1)%digit])
